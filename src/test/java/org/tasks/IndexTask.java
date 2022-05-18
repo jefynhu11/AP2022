b0a9ed3d987d8f95e57d409ea4e31d2e8ed1806d
jefynhu11 <jefyx1@hotmail.com>
2022-05-18 15:03:55 -0300

    Coloquei validar e report

--- src/test/java/org/tasks/IndexTask.java
+++ src/test/java/org/tasks/IndexTask.java
@@ -1,7 +1,12 @@
 package org.tasks;
 
+import org.bson.assertions.Assertions;
+import org.framework.tools.ScreenshotFw;
+import org.framework.tools.reports.ReportFw;
 import org.openqa.selenium.WebDriver;
 import org.pageobjects.IndexAppObject;
+import org.testng.Assert;
+import org.testng.asserts.Assertion;
 
 public class IndexTask {
 
@@ -17,6 +22,8 @@ public class IndexTask {
         indexAppObject.getContactUsButton().click();
     }
     public void signIn() {
+        Assert.assertEquals("My Store", driver.getTitle());
+        ReportFw.test.pass("O principal na pagina Automation Practice.", ScreenshotFw.screenshotBase64(driver));
         indexAppObject.getSignInButton().click();
     }
     public void search(){
