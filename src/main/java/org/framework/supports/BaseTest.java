368bddfa46e89d117d38f16b2f77fc990673909f
jefynhu11 <jefyx1@hotmail.com>
2022-05-18 14:59:25 -0300

    Coloquei metodos report

--- src/main/java/org/framework/supports/BaseTest.java
+++ src/main/java/org/framework/supports/BaseTest.java
@@ -1,11 +1,12 @@
 package org.framework.supports;
 
 import org.framework.tools.reports.ReportFw;
-import org.framework.utils.ObjectsUtils;
 import org.framework.webdrivers.Driver;
 import org.framework.webdrivers.DriverManager;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+
+import java.io.IOException;
 
 import static org.framework.webdrivers.DriverFactory.driver;
 import static org.framework.webdrivers.DriverFactory.getBrowser;
@@ -15,17 +16,16 @@ public class BaseTest {
 //    @BeforeClass
     @BeforeMethod
     public static void setUp() {
-        ReportFw.configurarExtentReport();
+        ReportFw.initReports();
         driver = getBrowser(Driver.CHROME);
         DriverManager.setDriver(driver);
-//        driver.get("http://automationpractice.com/index.php?");
-        driver.get(ObjectsUtils.getPropertiesData("path", "url"));
     }
 
 //    @AfterClass
     @AfterMethod
-    public static void tearDown() {
-        ReportFw.closeReport();
+    public static void tearDown() throws IOException {
+        ReportFw.test.info("Terminou teste!");
+        ReportFw.flushReports();
         DriverManager.quit();
     }
 }
