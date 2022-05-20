17666f1d010cf4c2591a0b09eed02409d9bb1603
jefynhu11 <jefyx1@hotmail.com>
2022-05-20 11:07:42 -0300

    Alteração codigo para atualizar

--- src/main/java/org/framework/supports/Wait.java
+++ src/main/java/org/framework/supports/Wait.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 public class Wait {
@@ -21,36 +22,38 @@ public class Wait {
     }
 
     public WebElement waitPresenceElementLocated(By by){
-//        WebDriverWait wait = new WebDriverWait(driver, 20);
-        WebDriverWait wait = new WebDriverWait(driver, 25);
+//        WebDriverWait wait = new WebDriverWait(driver, 25);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(25));
         return wait.until(ExpectedConditions.presenceOfElementLocated(by));
     }
 
     public WebElement waitVisibilityElement(By by){
-        WebDriverWait wait = new WebDriverWait(driver, 20);
+//        WebDriverWait wait = new WebDriverWait(driver, 20);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
         return wait.until(ExpectedConditions.visibilityOfElementLocated(by));
     }
 
     public WebElement waitElementToBeClickable(By by){
-        WebDriverWait wait = new WebDriverWait(driver, 20);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
         return wait.until(ExpectedConditions.elementToBeClickable(by));
     }
 
     public WebElement waitLoadElement(WebElement element) {
         try {
-            return new WebDriverWait(driver, 20).until(ExpectedConditions.visibilityOf(element));
+//            return new WebDriverWait(driver, 20).until(ExpectedConditions.visibilityOf(element));
+            return new WebDriverWait(driver, Duration.ofSeconds(20)).until(ExpectedConditions.visibilityOf(element));
         } catch (Exception e) {
             return element;
         }
     }
 
     public Boolean waitStaleness(WebElement element){
-        WebDriverWait wait = new WebDriverWait(driver, 20);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
         return wait.until(ExpectedConditions.stalenessOf(element));
     }
 
     public Alert waitElementAlert(){
-        WebDriverWait wait = new WebDriverWait(driver, 20);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
         return wait.until(ExpectedConditions.alertIsPresent());
     }
 }
