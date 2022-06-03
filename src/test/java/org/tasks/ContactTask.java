d5c35d75cd63576afd91fe9cce609306247200bc
jefynhu11 <jefyx1@hotmail.com>
2022-06-03 15:56:18 -0300

    Adicionei report status informação

--- src/test/java/org/tasks/ContactTask.java
+++ src/test/java/org/tasks/ContactTask.java
@@ -1,5 +1,7 @@
 package org.tasks;
 
+import org.framework.tools.ScreenshotFw;
+import org.framework.tools.reports.ReportFw;
 import org.openqa.selenium.WebDriver;
 import org.pageobjects.ContactAppObject;
 import org.testng.Assert;
@@ -19,13 +21,18 @@ public class ContactTask {
         String home = System.getProperty("user.home");
         contactAppObject.getSubjectHeadingSelect().selectByValue("1");
         contactAppObject.getEmailTextField().sendKeys("Jeferson@gmail.com");
+        ReportFw.test.info("Preenche do campo email.", ScreenshotFw.screenshotBase64(driver));
         contactAppObject.getOrderReferenceTextField().sendKeys("OUTROS");
+        ReportFw.test.info("Preenche do campo outro referência.", ScreenshotFw.screenshotBase64(driver));
         contactAppObject.getAttachFileButton().sendKeys(home + File.separator +
                 "Pictures" + File.separator +
                 "Saved Pictures" + File.separator +
                 "unnamed.jpg");
+        ReportFw.test.info("Seleciona arquivo.", ScreenshotFw.screenshotBase64(driver));
         contactAppObject.getMessageTextField().sendKeys("Mensagem teste");
+        ReportFw.test.info("Preenche do campo mensagem.", ScreenshotFw.screenshotBase64(driver));
         contactAppObject.getSendButton().click();
         Assert.assertEquals("Your message has been successfully sent to our team.",contactAppObject.getMessageSuccessfullyText().getText());
+        ReportFw.test.pass("Enviou de mensagem com sucesso.", ScreenshotFw.screenshotBase64(driver));
     }
 }
